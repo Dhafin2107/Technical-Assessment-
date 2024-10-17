cbc7e2c4e3973134c1b83fef33cfb70318da6d0c
Dhafin Taufiqi <101368249+Dhafin2107@users.noreply.github.com>
2024-10-17 09:09:53 +0700

    mempersiapkan starter kit

---
+++ weather_app/lib/data/api_endpoints.dart
@@ -0,0 +1,4 @@
+const baseUrl = 'https://reqres.in';
+const listUsers = '$baseUrl/api/users?page=1&per_page=10';
+const getUser = '$baseUrl/api/users/2';
+const inputUser = '$baseUrl/api/users';
---
+++ weather_app/lib/domain/network_request.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+import '../data/api_endpoints.dart';
+import '../data/types.dart';
+
+
+class Request {
+  final Dio _dio = Dio();
+
+  Request() {
+    updateDioInterceptors();
+  }
+
+  /// Fungsi ini digunakan untuk memperbarui konfigurasi dio interceptors
+  void updateDioInterceptors() {
+    _dio.options = BaseOptions(
+      baseUrl: baseUrl,
+      receiveDataWhenStatusError: true,
+      validateStatus: (value) {
+        return value! <= 500;
+      },
+      connectTimeout: 30 * 1000, // 30 seconds
+      receiveTimeout: 30 * 1000, // 30 seconds
+      headers: {
+        'Accept': 'application/json',
+      },
+    );
+    _dio
+      ..interceptors.add(
+        LogInterceptor(
+          requestBody: kDebugMode ? true : false,
+          responseBody: kDebugMode ? true : false,
+          requestHeader: kDebugMode ? true : false,
+        ),
+      )
+      ..interceptors.add(
+        InterceptorsWrapper(
+          onError: (DioError e, handler) {
+            if (kDebugMode) {
+              print(e.response);
+            }
+            if (e.response?.statusCode == 401) {
+
+            }
+            return handler.next(e);
+          },
+          onResponse: (e, handler) {
+            
+            return handler.next(e);
+          },
+        ),
+      );
+  }
+
+  /// Fungsi ini digunakan untuk melakukan GET request
+  Future<Response> get(String path,
+      {JSON? queryParameters}) async {
+    return await _dio.get(path, queryParameters: queryParameters);
+  }
+
+  /// Fungsi ini digunakan untuk melakukan POST request
+  Future<Response> post(String path,
+      {Object? data,
+      JSON? queryParameters}) async {
+    return await _dio.post(path, queryParameters: queryParameters, data: data);
+  }
+
+  /// Fungsi ini digunakan untuk melakukan DELETE request
+  Future<Response> delete(String path,
+      {Object? data}) async {
+    return await _dio.delete(path, data: data);
+  }
+
+  /// Fungsi ini digunakan untuk melakukan PUT request
+  Future<Response> put(String path,
+      {Object? data,
+      JSON? queryParameters}) async {
+    return await _dio.put(path, data: data, queryParameters: queryParameters);
+  }
+
+
+  }
+
